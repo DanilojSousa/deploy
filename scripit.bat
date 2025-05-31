7981590341af76e3edeb1ced75a656053aed9af1
danjs1985 <danilojsadm@gmail.com>
2025-05-31 16:15:41 -0300

    primeiro commit

---
+++ scripit.bat
@@ -0,0 +1,19 @@
+#!/bin/bash"
+echo "Criando as imagens....."
+
+docker build -t danilojstecnologia/projeto-backend:1.0 backend/.
+docker build -t danilojstecnologia/projeto-database:1.0 database/.
+
+echo "Realizando push das imagens...."
+
+docker push danilojstecnologia/projeto-backend:1.0
+docker push danilojstecnologia/projeto-database:1.0 
+
+echo "Criano serviços no cluster kubernets..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
+
